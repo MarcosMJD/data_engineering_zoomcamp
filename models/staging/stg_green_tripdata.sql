7e4b1a28501a441923b9b06ac7db0bd860e7b9b0
Marcos Jimenez <marcos.davia@gmail.com>
2022-02-15 11:02:06 +0000

    Models and tests created

--- models/staging/stg_green_tripdata.sql
+++ models/staging/stg_green_tripdata.sql
@@ -1,5 +1,11 @@
 {{ config(materialized='view') }}
-
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by vendorid, lpep_pickup_datetime) as rn
+  from {{ source('staging','green_tripdata_partitioned') }}
+  where vendorid is not null 
+)
 select
 
    -- identifiers
@@ -26,14 +32,15 @@ select
     cast(mta_tax as numeric) as mta_tax,
     cast(tip_amount as numeric) as tip_amount,
     cast(tolls_amount as numeric) as tolls_amount,
+    cast(ehail_fee as numeric) as ehail_fee,
     cast(improvement_surcharge as numeric) as improvement_surcharge,
     cast(total_amount as numeric) as total_amount,
     cast(payment_type as integer) as payment_type,
     {{ get_payment_type_description('payment_type') }} as payment_type_description,
     cast(congestion_surcharge as numeric) as congestion_surcharge
 
-from {{ source('staging', 'external_green_tripdata') }}
-where vendorid is not null
+from tripdata
+where rn = 1
 
 -- dbt build --m <model.sql> --var 'is_test_run: false'
 {% if var('is_test_run', default=true) %}
